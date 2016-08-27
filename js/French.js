3a25635177db73e870718f60f0c1e33e2b5ddcee
Jennifer Dutton <jduttondesign@gmail.com>
2016-08-27 12:03:42 -0500

    edited French.js

---
+++ js/French.js
@@ -0,0 +1,18 @@
+//translator variable that is used in each of the four languages javascript
+var Translator = (function(translator) {
+//variable portuguese and its words that correspond with one another 
+  var french = {
+    "merry" : "Joyeux",
+    "christmas" : "Noel", 
+    "and" : "et", 
+    "happy" : "Bon", 
+    "new" : "Nouveau", 
+    "year" : "Annee"
+  }
+
+  translator.translatetoGerman = function(key) {
+    return words[key.toLowerCase()];
+  };
+
+  return translator;
+})(Translator);
