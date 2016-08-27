8ad4936657486557c77970a6dc258ed58c0cf968
Jennifer Dutton <jduttondesign@gmail.com>
2016-08-27 11:54:47 -0500

    Revert "Merge branch 'jd-french'"
    This reverts commit 5e05f5d1b733942f3da83b13a607932fe74695cb, reversing
    changes made to 4bb31937859744ed784c006afa5027cea56c0a1c.

--- js/DOMHandler.js
+++ js/DOMHandler.js
@@ -1,4 +1,3 @@
-
 var translateBtn = document.getElementById("translateBtn");
 var inputGreetingTA = document.getElementById("inputGreeting");
 var outputGreetingDiv = document.getElementById("outputGreeting");
@@ -15,26 +14,10 @@ function translate() {
       case "spanish":
         return Translator.translateToSpanish(key);
         break;
-      case "french":
-        return Translator.translateToFrench(key);
-        break;  
     }
   });
   Translator.setFinalGreeting(newGreetingArr.join(" "));
   outputGreetingDiv.innerHTML = Translator.getFinalGreeting();
 }
 
-function speak() {
-  var msg = new SpeechSynthesisUtterance();
-  var voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[10]; // Note: some voices don't support altering params
-  msg.voiceURI = 'native';
-  msg.volume = 1; // 0 to 1
-  msg.rate = 1; // 0.1 to 10
-  msg.pitch = 1; //0 to 2
-  msg.text = Translator.getFinalGreeting();
-  speechSynthesis.speak(msg);
-}
-
 translateBtn.addEventListener("click", translate);
-playBtn.addEventListener("click", speak)
